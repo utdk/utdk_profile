9d42771d8f13c672e0ea47707645de6d3fb7781f
Tyler Fahey <tfahey@austin.utexas.edu>
2017-08-24 16:47:18 -0500

    Update drush usage to look for arg

--- install-dev-modules.sh
+++ install-dev-modules.sh
@@ -1,10 +1,24 @@
 #!/usr/bin/env bash
-
-drush @utdk8-shared.local en features_ui -y
-drush @utdk8-shared.local en field_ui -y
-drush @utdk8-shared.local en devel -y
-drush @utdk8-shared.local en kint -y
-drush @utdk8-shared.local en views_ui -y
-drush @utdk8-shared.local en admin_toolbar -y
-drush @utdk8-shared.local en admin_toolbar_tools -y
-drush @utdk8-shared.local uli
+# This script allows passing of a drush alias
+# to make the ULI properly load the web browser
+# and offer potentially other advantages of using
+# the native drush alias. Otherwise, it can be
+# used with the @self fallback.
+# Example usage, with local alias set to @utdk8-shared.local:
+# bash install-dev-modules.sh @utdk8-shared.local
+if [ -z "$1" ]
+    then
+    DRUSH_ALIAS="@self"
+else
+    DRUSH_ALIAS=$1
+    echo $DRUSH_ALIAS
+fi
+cd web
+drush $DRUSH_ALIAS en features_ui -y
+drush $DRUSH_ALIAS en field_ui -y
+drush $DRUSH_ALIAS en devel -y
+drush $DRUSH_ALIAS en kint -y
+drush $DRUSH_ALIAS en views_ui -y
+drush $DRUSH_ALIAS en admin_toolbar -y
+drush $DRUSH_ALIAS en admin_toolbar_tools -y
+drush $DRUSH_ALIAS uli
