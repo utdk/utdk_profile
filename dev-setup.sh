373245b67c9e9d246531935e1cfd491e6be8fa7a
Tyler Fahey <tfahey@austin.utexas.edu>
2017-08-24 15:31:32 -0500

    Add a dev-setup.sh
    -This allows developers to use `bash dev-setup.sh` to properly remove the potentially outdated composer.lock file, and reinstall fresh.

---
+++ dev-setup.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#
+# Currently, this file simply copies "example." files in the docroot to
+# non-example equivalents. Run "sh setup.sh" and then "composer install"
+# In the future, it might be used for additional things!
+
+cp example.gitignore .gitignore
+cp example.composer.json composer.json
+rm composer.lock
+cp example.pantheon.yml pantheon.yml
+cp example.circle.yml circle.yml
+composer install
+cp composer.local example.composer.lock
+echo "Updated example.composer.lock."
