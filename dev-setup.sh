16e69d3e549c96d7d7c8a715970735cc8d651725
Tyler Fahey <tfahey@austin.utexas.edu>
2017-08-24 15:35:20 -0500

    Update comments

--- dev-setup.sh
+++ dev-setup.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 #
-# Currently, this file simply copies "example." files in the docroot to
-# non-example equivalents. Run "sh setup.sh" and then "composer install"
-# In the future, it might be used for additional things!
+# This script is mostly equivalent to setup.sh
+# The difference between this script
+# and setup.sh is that this does NOT copy the example.composer.lock.
+# Rather, it removes any present composer.lock, and then updates the
+# present example.composer.lock. This is to allow
+# composer dependencies we have set to "dev-master" that do not have
+# an explicit version are updated as needed by composer when testing,
+# and ultimately reduces the extra step of updating the example.composer.json
+# when the time comes to update it.
 
 cp example.gitignore .gitignore
 cp example.composer.json composer.json
