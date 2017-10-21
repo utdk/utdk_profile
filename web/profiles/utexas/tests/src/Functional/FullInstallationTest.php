2067c34232a02a7b5cd6101197434237b0d6a548
Polo Reynaga <lreynaga@austin.utexas.com>
2017-10-20 19:01:37 -0500

    Adding tests. Still figuring minimal test issue

--- web/profiles/utexas/tests/src/Functional/FullInstallationTest.php
+++ web/profiles/utexas/tests/src/Functional/FullInstallationTest.php
@@ -24,7 +24,8 @@ class FullInstallationTest extends WebTestBase {
   protected function installParameters() {
     $parameters = parent::installParameters();
     // Add specific installation form parameters here, e.g.:
-    // $parameters['forms']['utexas_select_extensions']['flex_page_enabled'] = 1;
+    $parameters['forms']['utexas_select_extensions']['utexas_enable_flex_page_content_type'] = 1;
+    $parameters['forms']['utexas_select_extensions']['utexas_enable_fp_editor_role'] = 1;
     return $parameters;
   }
 
@@ -32,7 +33,15 @@ class FullInstallationTest extends WebTestBase {
    * Tests routes info.
    */
   public function testFullInstallation() {
-    $this->assertTrue(1 === 1);
+    // Assert that Flex Page role is enabled.
+    $fp_role = \Drupal::moduleHandler()->moduleExists('utexas_role_flex_page_editor');
+    $this->assertTrue($fp_role);
+    // Assert that Flex Page CT is enabled.
+    $fp_ct = \Drupal::moduleHandler()->moduleExists('utexas_content_type_flex_page');
+    $this->assertTrue($fp_ct);
+    // Assert that LPN is enabled.
+    $lpn = \Drupal::moduleHandler()->moduleExists('layout_per_node');
+    $this->assertTrue($lpn);
   }
 
 }
--- web/profiles/utexas/tests/src/Functional/MinimalInstallationTest.php
+++ web/profiles/utexas/tests/src/Functional/MinimalInstallationTest.php
@@ -24,7 +24,8 @@ class MinimalInstallationTest extends WebTestBase {
   protected function installParameters() {
     $parameters = parent::installParameters();
     // Add specific installation form parameters here, e.g.:
-    // $parameters['forms']['utexas_select_extensions']['flex_page_enabled'] = 1;
+    $parameters['forms']['utexas_select_extensions']['utexas_enable_flex_page_content_type'] = FALSE;
+    $parameters['forms']['utexas_select_extensions']['utexas_enable_fp_editor_role'] = FALSE;
     return $parameters;
   }
 
@@ -32,7 +33,15 @@ class MinimalInstallationTest extends WebTestBase {
    * Tests routes info.
    */
   public function testMinimalInstallation() {
-    $this->assertTrue(1 === 1);
+    // Assert that Flex Page role is disabled.
+    $fp_role = \Drupal::moduleHandler()->moduleExists('utexas_role_flex_page_editor');
+    $this->assertFalse($fp_role);
+    // Assert that Flex Page CT is disabled.
+    $fp_ct = \Drupal::moduleHandler()->moduleExists('utexas_content_type_flex_page');
+    $this->assertFalse($fp_ct);
+    // Assert that LPN is disabled.
+    $lpn = \Drupal::moduleHandler()->moduleExists('layout_per_node');
+    $this->assertFalse($lpn);
   }
 
 }
