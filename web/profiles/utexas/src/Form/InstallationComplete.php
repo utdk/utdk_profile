1d30ed0ff865eb4610ae47e7d8719bede73d2385
Tyler Fahey <tfahey@austin.utexas.edu>
2017-10-24 08:44:14 -0500

    Add redirect to completion

--- web/profiles/utexas/src/Form/InstallationComplete.php
+++ web/profiles/utexas/src/Form/InstallationComplete.php
@@ -4,6 +4,7 @@ namespace Drupal\utexas\Form;
 
 use Drupal\Core\Form\FormBase;
 use Drupal\Core\Form\FormStateInterface;
+use Drupal\Core\Url;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 
 /**
@@ -34,6 +35,17 @@ class InstallationComplete extends FormBase {
       '#type' => 'actions',
       '#weight' => 0,
     ];
+    $redirect = $this->get_installer_redirect();
+    // The installer doesn't make it easy (possible?) to return a redirect
+    // response, so set a redirection META tag in the output.
+    $meta_redirect = [
+      '#tag' => 'meta',
+      '#attributes' => [
+        'http-equiv' => 'refresh',
+        'content' => '0;url=' . $redirect,
+      ],
+    ];
+    $form['#attached']['html_head'][] = [$meta_redirect, 'meta_redirect'];
     return $form;
   }
 
@@ -42,6 +54,21 @@ class InstallationComplete extends FormBase {
    */
   public function submitForm(array &$form, FormStateInterface $form_state) {
     return new RedirectResponse(\Drupal::urlGenerator()->generateFromRoute('<front>'));
+  }
+  /**
+   * Helper function to return a redirect object to the homepage.
+   * Implements hook_install_tasks_alter().
+   */
+  public function get_installer_redirect() {
+    $path = '<front>';
+    $redirect = Url::fromUri('internal:/' . $path);
+    // Explicitly set the base URL, if not previously set, to prevent weird
+    // redirection snafus.
+    $base_url = $redirect->getOption('base_url');
+    if (empty($base_url)) {
+      $redirect->setOption('base_url', $GLOBALS['base_url']);
+    }
+    return $redirect->setOption('absolute', TRUE)->toString();
   }
 
 }
