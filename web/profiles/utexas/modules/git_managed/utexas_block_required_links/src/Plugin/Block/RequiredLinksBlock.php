71b492eaafccecff2ea9002a9ff606c0e8f7657f
Michael J Marsello <mmarsello@austin.utexas.edu>
2018-08-30 14:21:10 -0500

    UDK8-424: Required links (#353)
    * provide required links plugin block
    
    * move plugin block to its own module
    
    * update package in .info

---
+++ web/profiles/utexas/modules/git_managed/utexas_block_required_links/src/Plugin/Block/RequiredLinksBlock.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Drupal\utexas_block_required_links\Plugin\Block;
+
+use Drupal\Core\Block\BlockBase;
+
+/**
+ * Provides a 'RequiredLinksBlock' block.
+ *
+ * @Block(
+ *  id = "required_links_block",
+ *  admin_label = @Translation("Required Links Block"),
+ * )
+ */
+class RequiredLinksBlock extends BlockBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function build() {
+    $required_links = [
+      'http://www.utexas.edu' => 'UT Austin Home',
+      'http://www.utexas.edu/emergency' => 'Emergency Information',
+      'http://www.utexas.edu/site-policies' => 'Site Policies',
+      'http://www.utexas.edu/web-accessibility-policy' => 'Web Accessibility Policy',
+      'http://www.utexas.edu/web-privacy-policy' => 'Web Privacy Policy',
+      'https://get.adobe.com/reader/' => 'Adobe Reader',
+    ];
+    $output = '<ul class="ut-menu--list required-links__list">';
+    foreach ($required_links as $url => $title) {
+      $output .= '<li class="required-links__list-item"><a href="' . $url . '" class="required-links__link">' . $title . '</a></li>';
+    }
+    $output .= '</ul>';
+    $build = [];
+    $build['required_links_block']['#markup'] = $output;
+
+    return $build;
+  }
+
+}
