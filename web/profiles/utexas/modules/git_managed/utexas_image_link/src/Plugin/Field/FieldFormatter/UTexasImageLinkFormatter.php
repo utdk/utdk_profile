83ebb2c5b0c4a2e852769a91aaaf09029f07849f
Mark Fullmer <mfullmer@gmail.com>
2018-12-17 08:20:50 -0600

    Properly retrieve MID & FID separately in field formatter

--- web/profiles/utexas/modules/git_managed/utexas_image_link/src/Plugin/Field/FieldFormatter/UTexasImageLinkFormatter.php
+++ web/profiles/utexas/modules/git_managed/utexas_image_link/src/Plugin/Field/FieldFormatter/UTexasImageLinkFormatter.php
@@ -25,13 +25,14 @@ class UTexasImageLinkFormatter extends FormatterBase {
    */
   public function viewElements(FieldItemListInterface $items, $langcode) {
     $elements = [];
+    $responsive_image_style_name = 'utexas_responsive_image_il';
     foreach ($items as $delta => $item) {
       if (!empty($item->link)) {
         $url = Url::fromUri($item->link);
         $link = $url->toString();
       }
-      if ($file = \Drupal::entityTypeManager()->getStorage('file')->load($item->image)) {
-        $responsive_image_style_name = 'utexas_responsive_image_il';
+      if ($media = \Drupal::entityTypeManager()->getStorage('media')->load($item->image)) {
+        $media_attributes = $media->get('field_utexas_media_image')->getValue();
         // Collect cache tags to be added for each item in the field.
         $responsive_image_style = \Drupal::entityTypeManager()->getStorage('responsive_image_style')->load($responsive_image_style_name);
         $image_styles_to_load = [];
@@ -44,25 +45,27 @@ class UTexasImageLinkFormatter extends FormatterBase {
         foreach ($image_styles as $image_style) {
           $cache_tags = Cache::mergeTags($cache_tags, $image_style->getCacheTags());
         }
-        $image = new \stdClass();
-        $image->title = NULL;
-        $media = \Drupal::entityTypeManager()->getStorage('media')->load($item->image);
-        $media_attributes = $media->get('field_utexas_media_image')->getValue();
-        $image->alt = $media_attributes[0]['alt'];
-        $image->entity = $file;
-        $image->uri = $file->getFileUri();
-        $image->width = NULL;
-        $image->height = NULL;
-        $image_render_array = [
-          '#theme' => 'responsive_image_formatter',
-          '#item' => $image,
-          '#item_attributes' => [],
-          '#responsive_image_style_id' => $responsive_image_style_name,
-          '#url' => $link ?? '',
-          '#cache' => [
-            'tags' => $cache_tags,
-          ],
-        ];
+        $image_render_array = [];
+        if ($file = \Drupal::entityTypeManager()->getStorage('file')->load($media_attributes[0]['target_id'])) {
+          $image = new \stdClass();
+          $image->title = NULL;
+          $image->alt = $media_attributes[0]['alt'];
+          $image->entity = $file;
+          $image->uri = $file->getFileUri();
+          $image->width = NULL;
+          $image->height = NULL;
+          $image_render_array = [
+            '#theme' => 'responsive_image_formatter',
+            '#item' => $image,
+            '#item_attributes' => [],
+            '#responsive_image_style_id' => $responsive_image_style_name,
+            '#url' => $link ?? '',
+            '#cache' => [
+              'tags' => $cache_tags,
+            ],
+          ];
+        }
+
         // Add the file entity to the cache dependencies.
         // This will clear our cache when this entity updates.
         $renderer = \Drupal::service('renderer');
