659f906ecd4b49ed3d998f344c569f413c3e00ed
Mark Fullmer <mfullmer@gmail.com>
2019-01-17 15:36:47 -0600

    Clear cached files in filesystem

--- scripts/d8-lando-cache-clear.sh
+++ scripts/d8-lando-cache-clear.sh
@@ -15,11 +15,16 @@ SQLSTART="mysql -h $HOST -P $PORT -u $USER $DATABASE"
 # Gather and destroy tables
 TABLES=$($SQLSTART -e 'SHOW TABLES LIKE "cache%"' | awk '{ print $1}' | grep -v '^Tables' )
 for t in $TABLES; do
-  echo "Dropping $t table from $DATABASE database..."
   # DO db-specific wiping
   $SQLSTART -e "DROP TABLE $t"
 done
 
 echo ""
-printf "${GREEN}Cache cleared!${DEFAULT_COLOR}"
+printf "${GREEN}Cache rebuild complete.${DEFAULT_COLOR}"
 echo ""
+
+## Remove Twig PHP cache (presumes a web docroot & sites/default setup)
+rm -rf web/sites/default/files/php/twig/*
+## Remove CSS & JS cached files.
+rm -rf web/sites/default/files/css/*
+rm -rf web/sites/default/files/js/*
