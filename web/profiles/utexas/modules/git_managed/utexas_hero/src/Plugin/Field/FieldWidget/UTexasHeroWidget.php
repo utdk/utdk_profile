85a296d88b895b871f53014a2242ddabb0cac7f9
Mark Fullmer <mfullmer@gmail.com>
2019-01-18 13:16:38 -0600

    Define Hero widget with all available form field elements

--- web/profiles/utexas/modules/git_managed/utexas_hero/src/Plugin/Field/FieldWidget/UTexasHeroWidget.php
+++ web/profiles/utexas/modules/git_managed/utexas_hero/src/Plugin/Field/FieldWidget/UTexasHeroWidget.php
@@ -23,15 +23,81 @@ class UTexasHeroWidget extends WidgetBase {
    * {@inheritdoc}
    */
   public function formElement(FieldItemListInterface $items, $delta, array $element, array &$form, FormStateInterface $form_state) {
-    $element['value'] = $element + [
+    $element['media'] = [
+      '#type' => 'media_library_element',
+      '#target_bundles' => ['utexas_image'],
+      '#delta' => $delta,
+      '#cardinality' => 1,
+      '#title' => t('Image'),
+      '#default_value' => isset($items[$delta]->media) ? $items[$delta]->media : 0,
+      '#description' => t('Image will be scaled and cropped to a 87:47 ratio. Upload an image with a minimum resolution of 2280x1232 pixels to maintain quality and avoid cropping.'),
+    ];
+    $element['heading'] = [
+      '#title' => $this->t('Heading'),
       '#type' => 'textfield',
-      '#default_value' => isset($items[$delta]->value) ? $items[$delta]->value : NULL,
-      '#size' => $this->getSetting('size'),
-      '#placeholder' => $this->getSetting('placeholder'),
-      '#maxlength' => $this->getFieldSetting('max_length'),
+      '#default_value' => isset($items[$delta]->heading) ? $items[$delta]->heading : NULL,
+      '#size' => '60',
+      '#description' => $this->t('Optional, but recommended to provide alternative textual explanation of the media.'),
+      '#maxlength' => 255,
+    ];
+    $element['subheading'] = [
+      '#title' => $this->t('Subheading'),
+      '#type' => 'textfield',
+      '#default_value' => isset($items[$delta]->subheading) ? $items[$delta]->subheading : NULL,
+      '#size' => '60',
+      '#description' => $this->t('Optional. Displays directly beneath the heading. For best appearance, use no more than 140 characters. Note: this field is only visible in hero styles 4, 5 and 6.'),
+      '#maxlength' => 255,
+    ];
+    $element['caption'] = [
+      '#title' => $this->t('Caption'),
+      '#type' => 'textfield',
+      '#default_value' => isset($items[$delta]->subheading) ? $items[$delta]->subheading : NULL,
+      '#size' => '60',
+      '#description' => $this->t('Optional text to display directly beneath the media.'),
+      '#maxlength' => 255,
+    ];
+    $element['credit'] = [
+      '#title' => $this->t('Credit'),
+      '#type' => 'textfield',
+      '#default_value' => isset($items[$delta]->subheading) ? $items[$delta]->subheading : NULL,
+      '#size' => '60',
+      '#description' => $this->t('Optional way to provide attribution, displayed directly beneath the media.'),
+      '#maxlength' => 255,
+    ];
+    $element['cta'] = [
+      '#type' => 'fieldset',
+      '#title' => t('Call to Action'),
+    ];
+    $element['cta']['link'] = [
+      '#type' => 'utexas_link_element',
+      '#default_value' => [
+        'url' => $items[$delta]->link_uri ?? '',
+        'title' => $items[$delta]->link_title ?? '',
+      ],
     ];
 
     return $element;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function massageFormValues(array $values, array $form, FormStateInterface $form_state) {
+    // This loop is through (potential) field instances.
+    foreach ($values as &$value) {
+      if (isset($value['media']['media_library_selection'])) {
+        // @see MediaLibraryElement.php
+        $value['media'] = $value['media']['media_library_selection'];
+      }
+      else {
+        $value['media'] = 0;
+      }
+      if (isset($value['cta']['link']['url'])) {
+        $value['link_uri'] = $value['cta']['link']['url'] ?? '';
+        $value['link_title'] = $value['cta']['link']['title'] ?? '';
+      }
+    }
+    return $values;
   }
 
 }
