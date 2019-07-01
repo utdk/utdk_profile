d10f0fa80e23d25415a53e987a10045b04e8c722
Mark Fullmer <mfullmer@gmail.com>
2019-07-01 09:27:36 -0500

    Improve logic of form error to prevent duplication

--- web/profiles/utexas/modules/git_managed/utexas_layouts/js/layout-builder-form-error.js
+++ web/profiles/utexas/modules/git_managed/utexas_layouts/js/layout-builder-form-error.js
@@ -2,12 +2,10 @@
     'use strict';
     Drupal.behaviors.layoutBuilderFormError = {
       attach: function () {
-        if ($("#layout-builder-modal form.layout-builder-configure-block div[role = 'alert']").length) {   
+        $("div#error-message-bottom").remove();
+        if ($("#layout-builder-modal form.layout-builder-configure-block div[role = 'alert']").length) {
           var errorMessage = $("#layout-builder-modal div[data-drupal-messages]").html();
           $("#layout-builder-modal").append('<div id="error-message-bottom">' + errorMessage + '</div>');
-        }
-        else {
-          $("div#error-message-bottom").remove();
         }
       }
     };
