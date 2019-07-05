a5f1741e6f47559edbddec3770b223a7a350e7e4
Mark Fullmer <mfullmer@gmail.com>
2019-07-05 16:45:38 -0500

    Add test coverage for remaining widgets with multiple deltas

--- web/profiles/utexas/tests/src/Traits/PromoListTestTrait.php
+++ web/profiles/utexas/tests/src/Traits/PromoListTestTrait.php
@@ -34,6 +34,12 @@ trait PromoListTestTrait {
     // Verify the custom "Add promo list item" button works.
     $page->pressButton('Add promo list item');
     $assert->assertWaitOnAjaxRequest();
+
+    // Verify multiple lists can be added.
+    // Verify that multiple resource collections can be added.
+    $page->pressButton('Add another item');
+    $assert->assertWaitOnAjaxRequest();
+
     $fieldsets = $page->findAll('css', 'div.field--type-utexas-promo-list details');
     foreach ($fieldsets as $fieldset) {
       $fieldset->click();
@@ -41,11 +47,15 @@ trait PromoListTestTrait {
 
     $this->submitForm([
       'info[0][value]' => 'Promo List Test',
-      'field_block_pl[0][headline]' => 'Promo List Headline',
-      'field_block_pl[0][promo_list_items][0][item][headline]' => 'List item 1',
-      'field_block_pl[0][promo_list_items][1][item][headline]' => 'List item 2',
-      'field_block_pl[0][promo_list_items][0][item][copy][value]' => 'Cras porta cursus fermentum. Integer orci nunc, lobortis non sem sit amet, lobortis blandit turpis.',
+      'field_block_pl[0][headline]' => 'Promo List 1 Headline',
+      'field_block_pl[0][promo_list_items][0][item][headline]' => 'List 1 item 1',
+      'field_block_pl[0][promo_list_items][1][item][headline]' => 'List 1 item 2',
+      'field_block_pl[0][promo_list_items][0][item][copy][value]' => 'Copy text for list 1 item 1',
       'field_block_pl[0][promo_list_items][0][item][link][url]' => 'https://promolist.test',
+      'field_block_pl[1][headline]' => 'Promo List 2 Headline',
+      'field_block_pl[1][promo_list_items][0][item][headline]' => 'List 2 item 1',
+      'field_block_pl[1][promo_list_items][0][item][copy][value]' => 'Copy text for list 2 item 1',
+      'field_block_pl[1][promo_list_items][0][item][link][url]' => '/node/1',
     ], 'Save');
     $assert->pageTextContains('Promo List Promo List Test has been created.');
 
@@ -58,15 +68,19 @@ trait PromoListTestTrait {
 
     $this->drupalGet('<front>');
     // Verify page output.
-    $assert->elementTextContains('css', 'h3.ut-headline--underline', 'Promo List Headline');
-    $assert->elementTextContains('css', 'h3.ut-headline', 'List item 1');
-    $assert->pageTextContains('List item 2');
-    $assert->pageTextContains('Cras porta cursus fermentum. Integer orci nunc, lobortis non sem sit amet, lobortis blandit turpis.');
+    $assert->elementTextContains('css', '#block-promolisttest div div:nth-child(1) h3.ut-headline--underline', 'Promo List 1 Headline');
+    $assert->elementTextContains('css', '#block-promolisttest div div:nth-child(2) h3.ut-headline--underline', 'Promo List 2 Headline');
+    $assert->pageTextContains('List 1 item 2');
+    $assert->pageTextContains('List 2 item 1');
+    $assert->pageTextContains('Copy text for list 1 item 1');
+    $assert->pageTextContains('Copy text for list 2 item 1');
     $assert->linkByHrefExists('https://promolist.test');
+    $assert->linkByHrefExists('node/1');
+
     // Verify responsive image is present within the link.
-    $assert->elementExists('css', 'a picture source');
+    $assert->elementExists('css', '.ut-promo-list-wrapper .promo-list:nth-child(1) a picture source');
     $expected_path = 'utexas_image_style_64w_64h/public/image-test.png';
-    $assert->elementAttributeContains('css', 'a[href^="https://promolist.test"] picture img', 'src', $expected_path);
+    $assert->elementAttributeContains('css', '.ut-promo-list-wrapper .promo-list:nth-child(1) a[href^="https://promolist.test"] picture img', 'src', $expected_path);
 
     // Set display to "Responsive".
     $this->drupalGet('admin/structure/block/manage/promolisttest');
--- web/profiles/utexas/tests/src/Traits/PromoUnitTestTrait.php
+++ web/profiles/utexas/tests/src/Traits/PromoUnitTestTrait.php
@@ -14,6 +14,11 @@ trait PromoUnitTestTrait {
     $assert = $this->assertSession();
     $page = $this->getSession()->getPage();
     $this->drupalGet('block/add/utexas_promo_unit');
+
+    // Verify the custom "Add promo list item" button works.
+    $page->pressButton('Add promo unit item');
+    $assert->assertWaitOnAjaxRequest();
+
     $fieldsets = $page->findAll('css', 'div.field--type-utexas-promo-unit details');
     foreach ($fieldsets as $fieldset) {
       $fieldset->click();
@@ -37,6 +42,10 @@ trait PromoUnitTestTrait {
       'field_block_pu[0][promo_unit_items][0][item][copy][value]' => 'Promo Unit 1 Copy',
       'field_block_pu[0][promo_unit_items][0][item][link][url]' => 'https://promounit.test',
       'field_block_pu[0][promo_unit_items][0][item][link][title]' => 'Promo Unit External Link',
+      'field_block_pu[0][promo_unit_items][1][item][headline]' => 'Promo Unit 2 Headline',
+      'field_block_pu[0][promo_unit_items][1][item][copy][value]' => 'Promo Unit 2 Copy',
+      'field_block_pu[0][promo_unit_items][1][item][link][url]' => '/node/1',
+      'field_block_pu[0][promo_unit_items][1][item][link][title]' => 'Promo Unit Internal Link',
     ], 'Save');
     $assert->pageTextContains('Promo Unit Promo Unit Test has been created.');
 
@@ -50,13 +59,16 @@ trait PromoUnitTestTrait {
     $this->drupalGet('<front>');
     // Verify page output.
     $assert->elementTextContains('css', 'h3.ut-headline--underline', 'Promo Unit Container Headline');
-    $assert->elementTextContains('css', 'h3.ut-headline', 'Promo Unit 1 Headline');
-    $assert->pageTextContains('Promo Unit 1 Copy');
+    $assert->elementTextContains('css', '.utexas-promo-unit:nth-child(2) h3.ut-headline', 'Promo Unit 1 Headline');
+    $assert->elementTextContains('css', '.utexas-promo-unit:nth-child(3) h3.ut-headline', 'Promo Unit 2 Headline');
+    $assert->elementTextContains('css', '.utexas-promo-unit:nth-child(2)', 'Promo Unit 1 Copy');
+    $assert->elementTextContains('css', '.utexas-promo-unit:nth-child(3)', 'Promo Unit 2 Copy');
     $assert->linkByHrefExists('https://promounit.test');
+    $assert->linkByHrefExists('/node/1');
     // Verify responsive image is present within the link.
-    $assert->elementExists('css', 'a picture source');
+    $assert->elementExists('css', '.utexas-promo-unit:nth-child(2) a picture source');
     $expected_path = 'utexas_image_style_176w_112h/public/image-test.png';
-    $assert->elementAttributeContains('css', 'a[href^="https://promounit.test"] picture img', 'src', $expected_path);
+    $assert->elementAttributeContains('css', '.utexas-promo-unit:nth-child(2) a[href^="https://promounit.test"] picture img', 'src', $expected_path);
 
     // Set display to "Portrait".
     $this->drupalGet('admin/structure/block/manage/promounittest');
