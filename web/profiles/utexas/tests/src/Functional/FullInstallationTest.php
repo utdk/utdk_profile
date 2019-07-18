cafbe93791a9dbf5d92a612c8cab0f6b485f8713
Mark Fullmer <mfullmer@gmail.com>
2019-07-18 13:36:00 -0500

    Enable iframe_title_filter, responsive_tables_filter, and pathologic

--- web/profiles/utexas/tests/src/Functional/FullInstallationTest.php
+++ web/profiles/utexas/tests/src/Functional/FullInstallationTest.php
@@ -68,8 +68,84 @@ class FullInstallationTest extends BrowserTestBase {
     $this->assertEqual($timezone, 'America/Chicago');
     $this->assertEqual($country, 'US');
     // Assert Flex HTML elements are default values.
+    $ckeditor_actual = $this->config('editor.editor.flex_html')->get('settings.toolbar');
+    $ckeditor_expected = [
+      'rows' => [
+        0 => [
+          0 => [
+            'name' => 'Formatting',
+            'items' => [
+              0 => 'Bold',
+              1 => 'Italic',
+              2 => 'Underline',
+              3 => 'Strike',
+              4 => 'HorizontalRule',
+              5 => 'SpecialChar',
+              6 => 'Superscript',
+              7 => 'Subscript',
+              8 => 'RemoveFormat',
+            ],
+          ],
+          1 => [
+            'name' => 'Linking',
+            'items' => [
+              0 => 'DrupalLink',
+              1 => 'DrupalUnlink',
+            ],
+          ],
+          2 => [
+            'name' => 'Lists',
+            'items' => [
+              0 => 'BulletedList',
+              1 => 'NumberedList',
+              2 => 'Outdent',
+              3 => 'Indent',
+            ],
+          ],
+          3 => [
+            'name' => 'Media',
+            'items' => [
+              0 => 'Blockquote',
+              1 => 'DrupalImage',
+              2 => 'Table',
+            ],
+          ],
+          4 => [
+            'name' => 'Block Formatting',
+            'items' => [
+              0 => 'Format',
+              1 => 'JustifyLeft',
+              2 => 'JustifyCenter',
+              3 => 'JustifyRight',
+              4 => 'JustifyBlock',
+            ],
+          ],
+          5 => [
+            'name' => 'Tools',
+            'items' => [
+              0 => 'Source',
+              1 => 'qualtricsbutton',
+            ],
+          ],
+          6 => [
+            'name' => 'Editing',
+            'items' => [
+              0 => 'Undo',
+              1 => 'Redo',
+              2 => 'Cut',
+              3 => 'Copy',
+              4 => 'Paste',
+              5 => 'PasteText',
+              6 => 'PasteFromWord',
+              7 => 'Maximize',
+            ],
+          ],
+        ],
+      ],
+    ];
+    $this->assertEqual($ckeditor_actual, $ckeditor_expected);
     $allowed_tags = $this->config('filter.format.flex_html')->get('filters.filter_html.settings.allowed_html');
-    $tags_to_test = '<a href hreflang class id role title aria-controls aria-haspopup aria-label aria-expanded aria-selected data-toggle data-slide media rel target> <abbr title class id role> <address class id role> <article class id role> <aside class id role> <audio class id role autoplay buffered controls loop muted preload src volume> <blockquote class id role> <br class id role> <button type class id role aria-label aria-expanded aria-controls aria-haspopup data-toggle data-target data-dismiss data-placement data-container> <caption class id role> <cite title class id role> <code class id role> <col class id role> <colgroup class id role> <del class id role> <details class id role> <dl class id role> <dt class id role> <dd class id role> <div role class id aria-label aria-labelledby aria-hidden data-ride data-dismiss data-toggle data-parent data-spy data-offset data-target tabindex> <em class id role> <figure class id role> <figcaption class id role> <footer class id role> <header class id role> <hr class id role> <h1 class id role> <h2 class id role> <h3 class id role> <h4 class id role> <h5 class id role> <h6 class id role> <img alt height width align class id role src data-entity-type data-entity-uuid data-align data-caption title> <i class id role> <li role class id aria-controls aria-current data-slide-to data-target> <mark class id role> <nav class id role aria-label> <ol class id role aria-labelledby> <p class id role> <pre class id role> <rowspan class id role> <section class id role> <small class id role> <span class id role aria-hidden> <source src type> <strong class id role> <sub class id role> <summary class id role> <sup class id role> <table class id role title> <tbody class id role> <td class id role colspan headers title> <tfoot class id role> <th colspan headers scope class id role> <thead class id role> <time class id role> <tr class id role> <track src sclang label default> <ul class id role background bgcolor aria-labelledby> <video width height controls autoplay buffered loop muted playsinline poster preload src>';
+    $tags_to_test = '<a href hreflang class id role title aria-controls aria-haspopup aria-label aria-expanded aria-selected data-toggle data-slide media rel target> <abbr title class id role> <address class id role> <article class id role> <aside class id role> <audio class id role autoplay buffered controls loop muted preload src volume> <blockquote class id role> <br class id role> <button type class id role aria-label aria-expanded aria-controls aria-haspopup data-toggle data-target data-dismiss data-placement data-container> <caption class id role> <cite title class id role> <code class id role> <col class id role> <colgroup class id role> <del class id role> <details class id role> <dl class id role> <dt class id role> <dd class id role> <div role class id aria-label aria-labelledby aria-hidden data-ride data-dismiss data-toggle data-parent data-spy data-offset data-target tabindex> <em class id role> <figure class id role> <figcaption class id role> <footer class id role> <header class id role> <hr class id role> <h1 class id role> <h2 class id role> <h3 class id role> <h4 class id role> <h5 class id role> <h6 class id role> <img alt height width align class id role src data-entity-type data-entity-uuid data-align data-caption title> <i class id role> <li role class id aria-controls aria-current data-slide-to data-target> <mark class id role> <nav class id role aria-label> <ol class id role aria-labelledby> <p class id role> <pre class id role> <rowspan class id role> <section class id role> <small class id role> <span class id role aria-hidden> <source src type> <strike class id role> <strong class id role> <sub class id role> <summary class id role> <sup class id role> <table class id role title> <tbody class id role> <td class id role colspan headers title> <tfoot class id role> <th colspan headers scope class id role> <thead class id role> <time class id role> <tr class id role> <track src sclang label default> <ul class id role background bgcolor aria-labelledby> <video width height controls autoplay buffered loop muted playsinline poster preload src>';
     $this->assertEqual($allowed_tags, $tags_to_test);
     // Assert Flex HTML filter are enabled.
     $filter_status = $this->config('filter.format.flex_html')->get('filters.filter_autop.status');
