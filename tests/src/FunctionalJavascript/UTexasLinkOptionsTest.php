8bedf60b6ae49629708e3bb7395e66ecd74f0647
Jeff Cardwell <jacardwell@utexas.edu>
2021-09-03 16:32:07 -0500

    1524 - UTexas Link Options Element strips query parameters and anchor fragments during processing (#1713)
    * Respect any link options which the UTexasLinkOption helper is not specifically supplying (Resolves #1524)
    
    * Prevent uncommon scenarios when unnecessary operations performed on routes cause PHP warnings
    
    * Add test coverage for link variations
    
    * Refresh methods using latest patch
    
    * Refactor method for clarity and future improvement
    
    * Account for missing 'title' key
    
    Co-authored-by: Mark Fullmer <mfullmer@gmail.com>

--- tests/src/FunctionalJavascript/UTexasLinkOptionsTest.php
+++ tests/src/FunctionalJavascript/UTexasLinkOptionsTest.php
@@ -51,9 +51,9 @@ class UTexasLinkOptionsTest extends WebDriverTestBase {
   }
 
   /**
-   * Test all link options provided by the UTexasLinkOptions widget.
+   * Test link icons provided by the UTexasLinkOptions widget.
    */
-  public function testFlexHtml() {
+  public function testIcons() {
     $this->getSession()->resizeWindow(900, 2000);
     $assert = $this->assertSession();
     $page = $this->getSession()->getPage();
@@ -113,7 +113,6 @@ class UTexasLinkOptionsTest extends WebDriverTestBase {
     $page->pressButton('Save layout');
     $assert->pageTextContains('The layout override has been saved.');
 
-    $this->createScreenshot('test.png');
     // Delta 0 is present, links to an external site, shows an external icon,
     // and opens in a new tab.
     $this->assertRaw('<a href="https://www.utexas.edu" rel="noopener noreferrer" class="ut-cta-link--external ut-link" target="_blank">Link Number 1</a>');
@@ -145,6 +144,87 @@ class UTexasLinkOptionsTest extends WebDriverTestBase {
     $assert->fieldValueEquals('field_block_ql[0][links][1][options][attributes][class]', '0');
     // Assert former second link is now gone.
     $assert->pageTextNotContains('Link Number 2');
+
+    // CRUD: DELETE.
+    $this->drupalGet('admin/content/block-content');
+    $page->findLink($block_name)->click();
+    $page->clickLink('Delete');
+    $page->pressButton('Delete');
+    $this->drupalGet('admin/structure/block/block-content');
+    $assert->pageTextNotContains($block_name);
+
+    // TEST CLEANUP //
+    // Remove test page.
+    $storage_handler = \Drupal::entityTypeManager()->getStorage("node");
+    $entities = $storage_handler->loadMultiple([$flex_page]);
+    $storage_handler->delete($entities);
+  }
+
+  /**
+   * Test URL variations to make sure they're processed correctly.
+   */
+  public function testUrls() {
+    $this->getSession()->resizeWindow(900, 2000);
+    $assert = $this->assertSession();
+    $page = $this->getSession()->getPage();
+
+    // Create a Flex Page.
+    $flex_page = $this->createFlexPage();
+
+    // CRUD: CREATE.
+    $block_type = 'Quick Links';
+    $block_name = 'Link URLs Test';
+    $this->drupalGet('admin/content/block-content');
+    $this->clickLink('Add custom block');
+    $this->clickLink($block_type);
+
+    // Add two additional link inputs.
+    $page->pressButton('Add link');
+    $this->assertNotEmpty($assert->waitForElementVisible('named', [
+      'id_or_name',
+      'field_block_ql[0][links][1][uri]',
+    ]));
+    $assert->elementExists('css', '.js-form-item-field-block-ql-0-links-1-uri');
+
+    $page->pressButton('Add link');
+    $this->assertNotEmpty($assert->waitForElementVisible('named', [
+      'id_or_name',
+      'field_block_ql[0][links][2][uri]',
+    ]));
+    $assert->elementExists('css', '.js-form-item-field-block-ql-0-links-2-uri');
+
+    $this->submitForm([
+      'info[0][value]' => $block_name,
+      'field_block_ql[0][links][0][title]' => 'Internal link with anchor',
+      'field_block_ql[0][links][0][uri]' => '/node/' . $flex_page . '#anchor',
+      'field_block_ql[0][links][1][title]' => 'Internal link with query',
+      'field_block_ql[0][links][1][uri]' => '/node/' . $flex_page . '?query=1&search=test',
+      'field_block_ql[0][links][2][title]' => 'Link to front with query and anchor',
+      'field_block_ql[0][links][2][uri]' => '/#anchor?query=1&search=test',
+    ], 'Save');
+    $assert->pageTextContains($block_type . ' ' . $block_name . ' has been created.');
+
+    // Place the block on the Flex page.
+    $this->drupalGet('node/' . $flex_page . '/layout');
+    $this->clickLink('Add block');
+    $this->assertNotEmpty($assert->waitForText('Create custom block'));
+    $this->clickLink($block_name);
+    $this->assertNotEmpty($assert->waitForElementVisible('named', [
+      'id_or_name',
+      'layout-builder-modal',
+    ]));
+    $page->pressButton('Add block');
+    $this->assertNotEmpty($assert->waitForText('You have unsaved changes'));
+    $page->pressButton('Save layout');
+    $assert->pageTextContains('The layout override has been saved.');
+
+    // Delta 0 is present, links to an external site, shows an external icon,
+    // and opens in a new tab.
+    $this->assertRaw('<a href="/test-flex-page#anchor" class="ut-link">Internal link with anchor</a>');
+    // Delta 1 is present, links to an internal page, and has a lock.
+    $this->assertRaw('<a href="/test-flex-page?query=1&amp;search=test" class="ut-link">Internal link with query</a>');
+    // Delta 2 is present, links to the front page, and has a caret.
+    $this->assertRaw('<a href="/#anchor?query=1&amp;search=test" class="ut-link">Link to front with query and anchor</a>');
 
     // CRUD: DELETE.
     $this->drupalGet('admin/content/block-content');
