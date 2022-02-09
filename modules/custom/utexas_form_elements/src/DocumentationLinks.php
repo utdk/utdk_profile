a2b0ab4a0e22e4f992d61974bab5ca78631e8c05
Mark Fullmer <jfullmer@austin.utexas.edu>
2022-02-09 11:03:09 -0600

    1830 - Add link to docs for custom components from the block configuration form (#1843)
    * Add link to docs for custom components from the block configuration form (Resolves #1830)
    
    * Add link options library to all forms that add documentation

---
+++ modules/custom/utexas_form_elements/src/DocumentationLinks.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Drupal\utexas_form_elements;
+
+/**
+ * Provides a location for documentation references.
+ */
+class DocumentationLinks {
+  /**
+   * {@inheritdoc}
+   */
+  public static $docLinks = [
+    'basic' => 'https://drupalkit.its.utexas.edu/docs/content/custom_content_components.html#rich-text-areas-including-images',
+    'feed_block' => 'https://www.drupal.org/project/feed_block',
+    'utexas_featured_highlight' => 'https://drupalkit.its.utexas.edu/docs/content/custom_content_components.html#featured-highlight',
+    'utexas_flex_content_area' => 'https://drupalkit.its.utexas.edu/docs/content/custom_content_components.html#flex-content-area',
+    'utexas_flex_list' => 'https://drupalkit.its.utexas.edu/docs/content/custom_content_components.html#flex-list',
+    'utexas_hero' => 'https://drupalkit.its.utexas.edu/docs/content/custom_content_components.html#hero',
+    'utexas_image_link' => 'https://drupalkit.its.utexas.edu/docs/content/custom_content_components.html#image-link',
+    'utexas_photo_content_area' => 'https://drupalkit.its.utexas.edu/docs/content/custom_content_components.html#photo-content-area',
+    'utexas_promo_list' => 'https://drupalkit.its.utexas.edu/docs/content/custom_content_components.html#promo-lists',
+    'utexas_promo_unit' => 'https://drupalkit.its.utexas.edu/docs/content/custom_content_components.html#promo-units',
+    'utexas_quick_links' => 'https://drupalkit.its.utexas.edu/docs/content/custom_content_components.html#quick-links',
+    'utexas_resources' => 'https://drupalkit.its.utexas.edu/docs/content/custom_content_components.html#resources',
+    'social_links' => 'https://drupalkit.its.utexas.edu/docs/content/custom_content_components.html#social-links',
+    'twitter_widget' => 'https://drupalkit.its.utexas.edu/docs/external_content/index.html#twitter-content',
+  ];
+
+}
