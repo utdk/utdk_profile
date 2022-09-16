b84ae0577609bde2472ae14e984be5450065c664
Jeff Cardwell <jacardwell@utexas.edu>
2022-09-15 20:29:23 -0500

    1979 - PHP 8 deprecation notices triggered by `utexas_media_types` (#1986)
    * Add FileUploadSanitizeNameEvent event subscriber
    
    * Remove file validate hook

---
+++ modules/custom/utexas_media_types/src/EventSubscriber/FileUploadSubscriber.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace Drupal\utexas_media_types\EventSubscriber;
+
+use Drupal\Core\File\Event\FileUploadSanitizeNameEvent;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Drupal\utexas_media_types\TransliterateName;
+
+/**
+ * Event subscriber for file uploads.
+ */
+class FileUploadSubscriber implements EventSubscriberInterface {
+
+  /**
+   * A filename transliterate service instance.
+   *
+   * @var \Drupal\utexas_media_types\TransliterateName
+   */
+  protected $transliterateNameService;
+
+  /**
+   * FileUploadSubscriber constructor.
+   *
+   * @param \Drupal\utexas_media_types\TransliterateName $transliterate_name_service
+   *   The TransliterateName service.
+   */
+  public function __construct(TransliterateName $transliterate_name_service) {
+    $this->transliterateNameService = $transliterate_name_service;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function getSubscribedEvents() {
+    $events[FileUploadSanitizeNameEvent::class][] = ['transliterateName', -100];
+    return $events;
+  }
+
+  /**
+   * Transliterates the upload's filename.
+   *
+   * @param \Drupal\Core\File\Event\FileUploadSanitizeNameEvent $event
+   *   File upload sanitize name event.
+   */
+  public function transliterateName(FileUploadSanitizeNameEvent $event): void {
+    $filename = $event->getFilename();
+    $filename = $this->transliterateNameService->transliterateFilename($filename);
+    $event->setFilename($filename);
+  }
+
+}
