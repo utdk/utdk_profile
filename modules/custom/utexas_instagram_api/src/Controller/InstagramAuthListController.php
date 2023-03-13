5261dad3350d36ea9ca8a150e48736d35b1758f5
Marcus L Marler <marcus.marler@austin.utexas.edu>
2023-03-13 11:40:29 -0500

    2114 - New component: Instagram feed module part 1
    Co-authored-by: jmf3658 <jfullmer@austin.utexas.edu>

---
+++ modules/custom/utexas_instagram_api/src/Controller/InstagramAuthListController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace Drupal\utexas_instagram_api\Controller;
+
+use Drupal\Core\Controller\ControllerBase;
+use Symfony\Component\HttpFoundation\Request;
+use Drupal\utexas_instagram_api\UTexasInstagramApi;
+
+/**
+ * Defines a controller to list entity instances.
+ */
+class InstagramAuthListController extends ControllerBase {
+
+  /**
+   * Provides the listing page for the account authorization entity type.
+   *
+   * Note that we are extending ControllerBase rather than
+   * Drupal\Core\Entity\Controller\EntityListController. This is because the
+   * listing() method found in EntityListController is "not compatible" with our
+   * new one here.
+   *
+   * @param \Symfony\Component\HttpFoundation\Request $request
+   *   The request.
+   *
+   * @return array
+   *   A render array as expected by
+   *   \Drupal\Core\Render\RendererInterface::render().
+   */
+  public function listing(Request $request = NULL) {
+
+    // Get URL paramters following successful Instagram Account OAuth request.
+    $code = $request->query->get('code', NULL);
+    $state = $request->query->get('state', NULL);
+
+    if (!empty($code) && !empty($state)) {
+      $entities = $this->entityTypeManager()->getStorage('utexas_ig_auth')->loadByProperties(['id' => $state]);
+      $ig_auth = reset($entities);
+
+      if (!empty($ig_auth)) {
+        $instagram_request = new UTexasInstagramApi($ig_auth->id());
+        $instagram_request->setToken($code);
+        $instagram_request->getOauthAccessToken();
+      }
+    }
+
+    return $this->entityTypeManager()->getListBuilder('utexas_ig_auth')->render();
+  }
+
+}
