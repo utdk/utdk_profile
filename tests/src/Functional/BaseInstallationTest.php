339b6c282e37e2cd82acaf3336fd8984f8fea674
Marcus L Marler <marcus.marler@austin.utexas.edu>
2023-03-16 11:20:13 -0500

    2146 - Display CKEditor 5 toolbar items on two lines (#2150)
    * Add line wrap
    
    * Update functional test

--- tests/src/Functional/BaseInstallationTest.php
+++ tests/src/Functional/BaseInstallationTest.php
@@ -115,7 +115,7 @@ class BaseInstallationTest extends BrowserTestBase {
           'urlembed',
           'qualtrics',
           'blockQuote',
-          '|',
+          '-',
           'heading',
           '|',
           'specialCharacters',
