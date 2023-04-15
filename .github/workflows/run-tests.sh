70aef8139c639f7a4ca4edd47f7a9880e88da11b
jmf3658 <jfullmer@austin.utexas.edu>
2023-04-15 14:27:35 -0500

    Use absolute path in test execution

--- .github/workflows/run-tests.sh
+++ .github/workflows/run-tests.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -ex
 
 ## Run tests...
-su -s /bin/bash -c 'BROWSERTEST_CACHE_DB=1 /var/www/html/utdk_scaffold/vendor/bin/phpunit -c /var/www/html/.github/workflows/.fixtures/functional-js.phpunit.xml --stop-on-failure --testsuite=functional-javascript --verbose --debug web/profiles/utexas/tests/src/FunctionalJavascript' www-data
-su -s /bin/bash -c 'BROWSERTEST_CACHE_DB=1 /var/www/html/utdk_scaffold/vendor/bin/phpunit -c /var/www/html/utdk_scaffold/web/core/phpunit.xml.dist --stop-on-failure --testsuite=functional --verbose --debug web/profiles/utexas/tests/src/FunctionalJavascript' www-data
+su -s /bin/bash -c 'BROWSERTEST_CACHE_DB=1 /var/www/html/utdk_scaffold/vendor/bin/phpunit -c /var/www/html/.github/workflows/.fixtures/functional-js.phpunit.xml --stop-on-failure --testsuite=functional-javascript --verbose --debug /var/www/html/utdk_scaffold/web/profiles/utexas/tests/src/FunctionalJavascript' www-data
+su -s /bin/bash -c 'BROWSERTEST_CACHE_DB=1 /var/www/html/utdk_scaffold/vendor/bin/phpunit -c /var/www/html/utdk_scaffold/web/core/phpunit.xml.dist --stop-on-failure --testsuite=functional --verbose --debug /var/www/html/utdk_scaffold/web/profiles/utexas/tests/src/FunctionalJavascript' www-data
