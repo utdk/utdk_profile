99da1997635d9be6de3ec3699cd1529d6d33f0ea
Mark Fullmer <jfullmer@austin.utexas.edu>
2023-04-26 11:18:42 -0500

    2176 - [PHP 8] [xmlsitemap] TypeError: strpos(): Argument #1 ($haystack) must be of type string, array given in strpos() (#2203)
    * Convert incorrectly migrated metatags robots array to string
    
    * Skip processing if there are no rows in the metatags tables

--- src/InstallationHelper.php
+++ src/InstallationHelper.php
@@ -51,4 +51,67 @@ class InstallationHelper {
     $block->save();
   }
 
+  /**
+   * Convert incorrectly migrated metatags robots array to string.
+   */
+  public static function normalizeRobotsMetatags() {
+    $connection = \Drupal::database();
+    // Fix both the current node data and all revisions.
+    $tables = [
+      'node_revision__field_flex_page_metatags',
+      'node__field_flex_page_metatags',
+    ];
+    foreach ($tables as $table) {
+      $query = $connection->select($table, 'n');
+      $query->fields('n', [
+        'entity_id',
+        'revision_id',
+        'delta',
+        'field_flex_page_metatags_value',
+      ]);
+      $result = $query->execute();
+      $results = $result->fetchAll();
+      if (!$results || empty($results)) {
+        continue;
+      }
+      foreach ($results as $metatags) {
+        $metatags_array = unserialize($metatags->field_flex_page_metatags_value);
+        if (!isset($metatags_array['robots'])) {
+          // There are no robots declarations. Move on.
+          continue;
+        }
+        if (!is_array($metatags_array['robots'])) {
+          // The data is already in the correct string format. Move on.
+          continue;
+        }
+        $new_robots = [];
+        // Retrieve any robots declarations that are not 0 and put them in a
+        // comma-separated string.
+        // Previous format ['nofollow' => 'nofollow', 'noindex' => 'noindex'].
+        // New format: "nofollow, noindex".
+        foreach ($metatags_array['robots'] as $key => $value) {
+          if ($value !== 0) {
+            $new_robots[] = $key;
+          }
+        }
+        if (!empty($new_robots)) {
+          $metatags_array['robots'] = implode(", ", $new_robots);
+        }
+        else {
+          unset($metatags_array['robots']);
+        }
+        // Save the new format to the database.
+        $new_metatags = serialize($metatags_array);
+        $connection->update($table)
+          ->fields([
+            'field_flex_page_metatags_value' => $new_metatags,
+          ])
+          ->condition('entity_id', $metatags->entity_id, '=')
+          ->condition('revision_id', $metatags->revision_id, '=')
+          ->condition('delta', $metatags->delta, '=')
+          ->execute();
+      }
+    }
+  }
+
 }
