87b98e13b9c99b2e28d499cb554e91af124e6390
Doug R Addison <daddison@austin.utexas.edu>
2024-03-08 13:58:02 -0600

    2328 - Add a custom frontpage view with admin links (#2473)
    * customize message on default frontpage
    
    * Update link to create flex page
    
    * Update no result and header content
    
    * Add custom views area handlers
    
    * Update route and permissions
    
    * Add new view at installation
    
    * Update install
    
    * Update install
    
    * Add slash
    
    * Update link titles and help text
    
    * Style admin links as buttons
    
    * Update demo link text
    
    * Fix typo
    
    * Remove classes
    
    * Update code comments
    
    * Remove reference to accessmanager
    
    * Update translation syntax
    
    ---------
    
    Co-authored-by: jmf3658 <jfullmer@austin.utexas.edu>

---
+++ modules/custom/utexas_form_elements/src/Plugin/views/area/SetSiteHomepage.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace Drupal\utexas_form_elements\Plugin\views\area;
+
+use Drupal\Core\Link;
+use Drupal\Core\Url;
+use Drupal\views\Plugin\views\area\AreaPluginBase;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+/**
+ * Defines an area plugin to display a link to set the site homepage.
+ *
+ * @ingroup views_area_handlers
+ *
+ * @ViewsArea("set_site_homepage")
+ */
+class SetSiteHomepage extends AreaPluginBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
+    return new static(
+      $configuration,
+      $plugin_id,
+      $plugin_definition,
+    );
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function render($empty = FALSE) {
+    $account = \Drupal::currentUser();
+    if (!$empty || !empty($this->options['empty'])) {
+      $link_options = [
+        'attributes' => [
+          'class' => [
+            'ut-btn--secondary',
+            'ut-cta-link--angle-right',
+          ],
+        ],
+      ];
+      $url = Url::fromRoute('system.site_information_settings');
+      $url->setOptions($link_options);
+      $link = Link::fromTextAndUrl($this->t('Set the site homepage'), $url)->toString();
+      $element = [
+        '#markup' => '<p>' . $link . '</p>',
+        '#access' =>
+        $account->hasPermission('administer site configuration'),
+      ];
+      return $element;
+    }
+    return [];
+  }
+
+}
