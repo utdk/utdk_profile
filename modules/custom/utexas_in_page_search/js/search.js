0a96717cc61ddfe92acbfab3ff6f3ae1009b60ec
mmarler <marcus.marler@austin.utexas.edu>
2025-02-13 10:13:40 -0600

    Fix js syntax errors

--- modules/custom/utexas_in_page_search/js/search.js
+++ modules/custom/utexas_in_page_search/js/search.js
@@ -5,38 +5,12 @@
  */
 
 Drupal.behaviors.inPageSearch = {
-  attach: function (context, drupalSettings) {
-    window.onload = function () {
-      const searchBox = document.getElementById("inPageSearchBox");
-      if (searchBox !== null) {
-        searchBox.addEventListener('keyup', function (event) {
-          if (event.key != 13) {
-            inPageSearch();
-          }
-        });
-        searchBox.addEventListener('keypress', function (event) {
-          if (event.key == 13) {
-            inPageSearch();
-          }
-        });
-        const searchField = document.getElementById("inPageSearchInput");
-        let params = new URLSearchParams(document.location.search);
-        let keyword = params.get("keyword");
-        if (keyword) {
-          searchField.value = keyword;
-          inPageSearch();
-        }
-        const searchResetButton = document.getElementById("inPageSearchResetButton");
-        searchResetButton.addEventListener('click', function (event) {
-          window.location = window.location.pathname;
-        });
-      }
-    }
+  attach: ((context, drupalSettings) => {
     function inPageSearch() {
       if (drupalSettings.in_page_search == null) {
         return false;
       }
-      const selector = '#' + drupalSettings.in_page_search.target + ' ' + drupalSettings.in_page_search.delimiter;
+      const selector = "#" + drupalSettings.in_page_search.target + " " + drupalSettings.in_page_search.delimiter;
       const container = document.querySelectorAll(selector);
       if (container == null) {
         return false;
@@ -50,11 +24,11 @@ Drupal.behaviors.inPageSearch = {
       for (var item of container) {
         if (item.matches(drupalSettings.in_page_search.delimiter)) {
           if (!item.getHTML().toLowerCase().includes(keyword.value.toLowerCase())) {
-            item.style.display = 'none';
+            item.style.display = "none";
             noResultsCount++;
           }
           else {
-            item.style.display = '';
+            item.style.display = "";
           }
         }
       }
@@ -65,5 +39,31 @@ Drupal.behaviors.inPageSearch = {
         noResults.style.display = "none";
       }
     }
-  }
+    window.onload = (() => {
+      const searchBox = document.getElementById("inPageSearchBox");
+      if (searchBox !== null) {
+        searchBox.addEventListener("keyup", ((event) => {
+          if (event.key !== 13) {
+            inPageSearch();
+          }
+        }))();
+        searchBox.addEventListener("keypress", ((event) => {
+          if (event.key == 13) {
+            inPageSearch();
+          }
+        }))();
+        const searchField = document.getElementById("inPageSearchInput");
+        let params = new URLSearchParams(document.location.search);
+        let keyword = params.get("keyword");
+        if (keyword) {
+          searchField.value = keyword;
+          inPageSearch();
+        }
+        const searchResetButton = document.getElementById("inPageSearchResetButton");
+        searchResetButton.addEventListener("click", ((event) => {
+          window.location = window.location.pathname;
+        }))();
+      }
+    })();
+  })(),
 };
