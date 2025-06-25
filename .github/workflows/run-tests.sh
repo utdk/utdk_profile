6dfd41e4de00ea79dd07a4300d5cd27badb56df3
jmf3658 <jfullmer@austin.utexas.edu>
2025-06-25 15:33:42 -0700

    Rework tests to work with PHPUnit 10+

--- .github/workflows/run-tests.sh
+++ .github/workflows/run-tests.sh
@@ -1,5 +1,6 @@
 #!/bin/bash -ex
 
 ## Run tests...
-su -s /bin/bash -c 'BROWSERTEST_CACHE_DB=1 /var/www/tests/utdk-project/vendor/bin/phpunit -c /var/www/tests/.github/workflows/.fixtures/functional-js.phpunit.xml --stop-on-failure --testsuite=functional-javascript --verbose --debug /var/www/tests/utdk-project/web/profiles/custom/utexas/tests/src/FunctionalJavascript' www-data
-su -s /bin/bash -c 'BROWSERTEST_CACHE_DB=1 /var/www/tests/utdk-project/vendor/bin/phpunit -c /var/www/tests/utdk-project/web/core/phpunit.xml.dist --stop-on-failure --testsuite=functional --verbose --debug /var/www/tests/utdk-project/web/profiles/custom/utexas/tests/src/Functional' www-data
+su -s /bin/bash -c '/var/www/tests/utdk-project/vendor/bin/phpunit -c /var/www/tests/utdk-project/web/core/phpunit.xml.dist --testdox /var/www/tests/utdk-project/web/profiles/custom/utexas/tests/src/Functional' www-data
+su -s /bin/bash -c '/var/www/tests/utdk-project/vendor/bin/phpunit -c /var/www/tests/.github/workflows/.fixtures/functional-js.phpunit.xml --testdox /var/www/tests/utdk-project/web/profiles/custom/utexas/tests/src/FunctionalJavascript' www-data
+
--- .github/workflows/utdk-tests-compose.sh
+++ .github/workflows/utdk-tests-compose.sh
@@ -11,13 +11,12 @@ gh auth setup-git --hostname $HOST
 FORTYACRES=$(gh api repos/$OWNER/forty_acres/tags --jq '.[0].name' --hostname $HOST)
 # Install stuff for UTDK site with composer...
 # Executed on host/Github Action Runner VM for ease of management of access to protected resources, file permissions, and performance...
-$COMPOSER create-project utexas/utdk-project:dev-develop --stability=dev --remove-vcs --no-install
+$COMPOSER create-project utexas/utdk-project:dev-d11 --stability=dev --remove-vcs --no-install
 cd utdk-project/upstream-configuration
 $COMPOSER remove utexas/utdk_profile --no-update
 cd ..
 $COMPOSER config repositories.utdk_profile vcs git@github.austin.utexas.edu:eis1-wcs/utdk_profile.git
 $COMPOSER config repositories.forty_acres vcs git@github.austin.utexas.edu:eis1-wcs/forty_acres.git
-$COMPOSER config repositories.utexas_qualtrics_filter vcs git@github.austin.utexas.edu:eis1-wcs/utexas_qualtrics_filter.git
 $COMPOSER config repositories.utdk_localdev vcs git@github.austin.utexas.edu:eis1-wcs/utdk_localdev.git
 $COMPOSER config --no-plugins allow-plugins.dealerdirect/phpcodesniffer-composer-installer true
 $COMPOSER config --no-plugins allow-plugins.phpstan/extension-installer true
@@ -25,7 +24,5 @@ $COMPOSER config --no-plugins allow-plugins.phpstan/extension-installer true
 $COMPOSER clear-cache
 $COMPOSER require utexas/utdk_profile:"dev-$BRANCH"  --no-update
 $COMPOSER require utexas/forty_acres:"dev-develop as $FORTYACRES" --no-update
-# Pin to Drush 12 while on PHP 8.1
-composer require drush/drush:^12 --no-update
-$COMPOSER require "drupal/core-dev":"^10" --ignore-platform-reqs
+$COMPOSER require "drupal/core-dev":"^11" --ignore-platform-reqs
 cat composer.json
