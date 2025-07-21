c05c4bbef21dbc0b29489b1dbdc5bfb2caec6e06
Doug Addison <daddison@austin.utexas.edu>
2025-07-21 09:20:48 -0500

    2871 - Append "Link opens in new window" to visible text in aria-label instead of overwriting (#2882)

--- js/linkAccessibility.js
+++ js/linkAccessibility.js
@@ -1,4 +1,4 @@
-(function ($, Drupal) {
+(($, Drupal) => {
   "use strict";
 
   /**
@@ -6,31 +6,30 @@
    *
    * Progressive enhancements to make links more accessible.
    */
-  Drupal.behaviors.utexasLinkAccessibility = {
-    attach: function (context, settings) {
-      $('body a').each(function () {
-        modifyLink(this);
-      });
-    }
-  }
-
   /**
    * If a link has target="_blank"/"new", append an aria-label.
    * @param {object} el The link element.
    */
   function modifyLink(el) {
-    let newWindowtext = "Link opens in new window";
-    var target = el.getAttribute('target');
-    if (target == '_blank' || target == 'new') {
-      var label = el.getAttribute('aria-label');
+    const newWindowtext = "Link opens in new window";
+    const target = el.getAttribute("target");
+    const text = el.innerText;
+    if (target === "_blank" || target === "new") {
+      let label = el.getAttribute("aria-label");
       if (label == null) {
-        label = newWindowtext;
+        label = `${text}; ${newWindowtext}`;
+      } else {
+        label = `${label}; ${newWindowtext}`;
       }
-      else {
-        label = label + '; ' + newWindowtext;
-      }
-      el.setAttribute('aria-label', label);
+      el.setAttribute("aria-label", label);
     }
   }
 
+  Drupal.behaviors.utexasLinkAccessibility = {
+    attach() {
+      $("body a").each(function handleLink() {
+        modifyLink(this);
+      });
+    }
+  };
 })(jQuery, Drupal);
