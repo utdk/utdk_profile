e2086ed952674039db9db7b39184a6922166eb01
jmf3658 <jfullmer@austin.utexas.edu>
2025-08-07 15:14:59 -0700

    Refactor automatic multidev GitHub action to be user-executed on demand (#2904)

--- .github/workflows/scripts/delete.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash -ex
-
-TERMINUS_CMD="$HOME/vendor/bin/terminus"
-
-# Enforce 11-charater limit on multidev names
-MULTIDEV="${BRANCH:0:11}"
-
-echo "Deleting $SITE.$MULTIDEV"
-$TERMINUS_CMD multidev:delete $SITE.$MULTIDEV --delete-branch -y
