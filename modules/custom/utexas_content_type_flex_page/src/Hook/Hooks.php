9b7e3d0825e449b3f881dd2a99a091a7450cb34f
jmf3658 <jfullmer@austin.utexas.edu>
2026-01-06 16:37:27 -0600

    Add string translation

--- modules/custom/utexas_content_type_flex_page/src/Hook/Hooks.php
+++ modules/custom/utexas_content_type_flex_page/src/Hook/Hooks.php
@@ -3,11 +3,14 @@
 namespace Drupal\utexas_content_type_flex_page\Hook;
 
 use Drupal\Core\Hook\Attribute\Hook;
+use Drupal\Core\StringTranslation\StringTranslationTrait;
 
 /**
  * Hook implementations.
  */
 class Hooks {
+
+  use StringTranslationTrait;
 
   /**
    * Implements hook_form_FORM_ID_alter().
@@ -22,11 +25,11 @@ class Hooks {
     $message = '';
     switch ($form_id) {
       case 'node_utexas_flex_page_edit_form':
-        $message = t('<ul><li>To set page metadata and control the display of the page title & breadcrumbs, use the vertical tabs on this page.</li><li>To add content, click the "Layout" tab, above.</li></ul>');
+        $message = $this->t('<ul><li>To set page metadata and control the display of the page title & breadcrumbs, use the vertical tabs on this page.</li><li>To add content, click the "Layout" tab, above.</li></ul>');
         break;
 
       case 'node_utexas_flex_page_form':
-        $message = t('To add content to this page, first add a title and save this form. A "Layout" tab will then appear, from which you can add content.');
+        $message = $this->t('To add content to this page, first add a title and save this form. A "Layout" tab will then appear, from which you can add content.');
         break;
     }
     if ($message !== '') {
@@ -36,4 +39,5 @@ class Hooks {
       ];
     }
   }
+
 }
