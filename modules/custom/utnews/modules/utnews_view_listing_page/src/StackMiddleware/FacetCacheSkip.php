238e3a91c8b81f7063b17e33d200cee85f0a28b9
jmf3658 <jfullmer@austin.utexas.edu>
2026-08-21 16:09:45 -0500

    Further simplify cache bypass

--- modules/custom/utnews/modules/utnews_view_listing_page/src/StackMiddleware/FacetCacheSkip.php
+++ modules/custom/utnews/modules/utnews_view_listing_page/src/StackMiddleware/FacetCacheSkip.php
@@ -6,18 +6,14 @@ use Drupal\Component\Utility\UrlHelper;
 use Drupal\Core\Cache\CacheBackendInterface;
 use Drupal\Core\PageCache\RequestPolicyInterface;
 use Drupal\Core\PageCache\ResponsePolicyInterface;
-use Drupal\Core\Url;
 use Drupal\page_cache\StackMiddleware\PageCache;
 use Symfony\Component\EventDispatcher\EventDispatcherInterface;
-use Symfony\Component\EventDispatcher\GenericEvent;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\HttpKernelInterface;
 
 /**
- * Executes the page caching before the main kernel takes over the request.
- *
- * Ignore query parameters also.
+ * Do not use facets' 'f' query parameter in constructing Cache ID for /news.
  */
 class FacetCacheSkip extends PageCache {
 
@@ -36,20 +32,6 @@ class FacetCacheSkip extends PageCache {
   protected $definedQueryParameters = ['f'];
 
   /**
-   * The ignore action.
-   *
-   * @var string
-   */
-  protected $ignoreAction = 'ignore';
-
-  /**
-   * The cache ID as computed by core.
-   *
-   * @var string|null
-   */
-  protected ?string $coreCid = NULL;
-
-  /**
    * {@inheritdoc}
    */
   public function __construct(HttpKernelInterface $http_kernel, CacheBackendInterface $cache, RequestPolicyInterface $request_policy, ResponsePolicyInterface $response_policy, ?EventDispatcherInterface $event_dispatcher = NULL) {
@@ -58,25 +40,9 @@ class FacetCacheSkip extends PageCache {
   }
 
   /**
-   * Get defined query parameters.
-   *
-   * @return array
-   *   Ignored params.
-   */
-  protected function getDefinedQueryParameters() {
-    return $this->definedQueryParameters;
-  }
-
-  /**
    * {@inheritdoc}
    */
   protected function getCacheId(Request $request) {
-    // Once a cache ID is determined for the request, reuse it for the duration
-    // of the request. This ensures that when the cache is written, it is only
-    // keyed on request data that was available when it was read. For example,
-    // the request format might be NULL during cache lookup and then set during
-    // routing, in which case we want to key on NULL during writing, since that
-    // will be the value during lookups for subsequent requests.
     if (!isset($this->cid)) {
       $cleared = $this->clear($request->getRequestUri());
       $cid_parts = [
@@ -92,11 +58,11 @@ class FacetCacheSkip extends PageCache {
    * {@inheritdoc}
    */
   protected function get(Request $request, $allow_invalid = FALSE) {
-    $this->coreCid ??= parent::getCacheId($request);
+    $coreCid = parent::getCacheId($request);
     // The cid is cached once computed, so it has to be reset to recompute.
     $this->cid = NULL;
     $customCid = $this->getCacheId($request);
-    if ($customCid === $this->coreCid) {
+    if ($customCid === $coreCid) {
       // If cids are the same, we can hand over control right away.
       return parent::get($request);
     }
@@ -121,55 +87,24 @@ class FacetCacheSkip extends PageCache {
    */
   protected function clear($value) {
     $request_parts = UrlHelper::parse($value);
-
     if (($request_parts['path'] !== '/news')) {
       return $value;
     }
-
     if (empty($request_parts['query'])) {
       return $value;
     }
-
     $request_uri = '';
-
     if (!empty($request_parts['path'])) {
       $request_uri .= $request_parts['path'];
     }
-
     // Remove the query arguments that are excluded.
-    $request_query = $this->removeExcludedQueryParameters($request_parts['query']);
-
-    // Allow modules to further normalize the query, e.g. filter values
-    // inside bracket arrays like `?f[0]=alias:value` by their prefix.
-    if ($this->eventDispatcher !== NULL) {
-      $event = new GenericEvent(NULL, [
-        'query' => $request_query,
-        'parts' => $request_parts,
-      ]);
-      $this->eventDispatcher->dispatch($event, 'utnews.facet_skip');
-      $request_query = (array) $event->getArgument('query');
-    }
-
+    $request_query = UrlHelper::filterQueryParameters($request_parts['query'], $this->definedQueryParameters);
     if (!empty($request_query)) {
       // Sort the query parameters to minimize cache variants.
       ksort($request_query);
       $request_uri .= '?' . UrlHelper::buildQuery($request_query);
     }
-
     return $request_uri;
-  }
-
-  /**
-   * Remove the excluded query parameters.
-   *
-   * @param array $query_parts
-   *   The query parts.
-   *
-   * @return array
-   *   The modified query parts.
-   */
-  protected function removeExcludedQueryParameters(array $query_parts) {
-    return UrlHelper::filterQueryParameters($query_parts, $this->getDefinedQueryParameters());
   }
 
 }
