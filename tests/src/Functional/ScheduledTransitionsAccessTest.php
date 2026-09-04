026695a25891cbf321527bd7793d8246a8647585
Doug Addison <daddison@austin.utexas.edu>
2026-09-04 08:36:01 -0500

    Add tests

---
+++ tests/src/Functional/ScheduledTransitionsAccessTest.php
@@ -0,0 +1,102 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Drupal\Tests\utexas\Functional;
+
+use Drupal\node\Entity\Node;
+use Drupal\scheduled_transitions\ScheduledTransitionsPermissions as Permissions;
+
+/**
+ * Tests that the Scheduled Transitions UI is gated by permission.
+ *
+ * @group utexas
+ * @group scheduled_transitions
+ */
+class ScheduledTransitionsAccessTest extends FunctionalTestBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  protected static $modules = ['scheduled_transitions'];
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function setUp(): void {
+    parent::setUp();
+    $this->drupalPlaceBlock('local_tasks_block');
+
+    // The 'page' bundle is on standard_workflow by default (see
+    // profile config/install/workflows.workflow.standard_workflow.yml), but
+    // Scheduled Transitions' own content-moderation-support access check
+    // additionally requires the bundle to be listed here.
+    \Drupal::configFactory()->getEditable('scheduled_transitions.settings')
+      ->set('bundles', [
+        ['entity_type' => 'node', 'bundle' => 'page'],
+      ])
+      ->save(TRUE);
+  }
+
+  /**
+   * A user without any scheduling permission sees and can access nothing.
+   */
+  public function testAccessDeniedWithoutPermission(): void {
+    $node = Node::create([
+      'type' => 'page',
+      'title' => $this->randomMachineName(),
+      'moderation_state' => 'published',
+    ]);
+    $node->save();
+
+    $user = $this->drupalCreateUser(['access content']);
+    $this->drupalLogin($user);
+
+    $this->drupalGet($node->toUrl());
+    $this->assertSession()->statusCodeEquals(200);
+    $this->assertSession()->elementTextNotContains('css', 'nav.tabs', 'Scheduled transitions');
+
+    $this->assertForbidden($node->toUrl('scheduled_transitions'));
+    $this->assertForbidden($node->toUrl('scheduled_transition_add'));
+  }
+
+  /**
+   * A user with the bundle-specific permissions sees and can use the UI.
+   */
+  public function testAccessGrantedWithPermission(): void {
+    $node = Node::create([
+      'type' => 'page',
+      'title' => $this->randomMachineName(),
+      'moderation_state' => 'published',
+    ]);
+    $node->save();
+
+    $user = $this->drupalCreateUser([
+      'access content',
+      Permissions::viewScheduledTransitionsPermission('node', 'page'),
+      Permissions::addScheduledTransitionsPermission('node', 'page'),
+    ]);
+    $this->drupalLogin($user);
+
+    $this->drupalGet($node->toUrl());
+    $this->assertSession()->statusCodeEquals(200);
+    $this->assertSession()->elementTextContains('css', 'nav.tabs', 'Scheduled transitions');
+
+    $this->assertAllowed($node->toUrl('scheduled_transitions'));
+    $this->assertAllowed($node->toUrl('scheduled_transition_add'));
+  }
+
+  /**
+   * The sitewide scheduled transitions report requires its own permission.
+   */
+  public function testAdminCollectionRequiresViewAllPermission(): void {
+    $user = $this->drupalCreateUser([]);
+    $this->drupalLogin($user);
+    $this->assertForbidden('/admin/content/scheduled-transitions');
+
+    $user = $this->drupalCreateUser(['view all scheduled transitions']);
+    $this->drupalLogin($user);
+    $this->assertAllowed('/admin/content/scheduled-transitions');
+  }
+
+}
---
+++ tests/src/Functional/ScheduledTransitionsHourRestrictTest.php
@@ -0,0 +1,130 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Drupal\Tests\utexas\Functional;
+
+use Drupal\node\Entity\Node;
+use Drupal\scheduled_transitions\Entity\ScheduledTransition;
+use Drupal\scheduled_transitions\ScheduledTransitionsPermissions as Permissions;
+
+/**
+ * Tests the top-of-the-hour restriction on scheduled transition forms.
+ *
+ * @group utexas
+ * @group scheduled_transitions
+ */
+class ScheduledTransitionsHourRestrictTest extends FunctionalTestBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  protected static $modules = ['scheduled_transitions_hour_restrict'];
+
+  /**
+   * A node on standard_workflow, shared across test methods.
+   *
+   * @var \Drupal\node\NodeInterface
+   */
+  protected $node;
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function setUp(): void {
+    parent::setUp();
+
+    \Drupal::configFactory()->getEditable('scheduled_transitions.settings')
+      ->set('bundles', [
+        ['entity_type' => 'node', 'bundle' => 'page'],
+      ])
+      ->save(TRUE);
+
+    $this->node = Node::create([
+      'type' => 'page',
+      'title' => $this->randomMachineName(),
+      'moderation_state' => 'draft',
+    ]);
+    $this->node->save();
+
+    $user = $this->drupalCreateUser([
+      'access content',
+      Permissions::viewScheduledTransitionsPermission('node', 'page'),
+      Permissions::addScheduledTransitionsPermission('node', 'page'),
+      Permissions::rescheduleScheduledTransitionsPermission('node', 'page'),
+      'use standard_workflow transition publish',
+    ]);
+    $this->drupalLogin($user);
+  }
+
+  /**
+   * Submitting the add form with a non-hour time is rejected.
+   */
+  public function testAddFormRejectsNonHourTime(): void {
+    $this->drupalGet($this->node->toUrl('scheduled_transition_add'));
+    $this->assertSession()->statusCodeEquals(200);
+    $this->assertSession()->pageTextContains('Scheduled transitions are only allowed at the top of the hour.');
+
+    $date = new \DateTime('+1 day');
+    $this->submitForm([
+      'revision' => 'latest_revision',
+      'transition' => 'publish',
+      // The 'on' datetime element posts as two sub-fields; Drupal only
+      // combines them into a single DrupalDateTime value during validation.
+      'on[date]' => $date->format('Y-m-d'),
+      'on[time]' => '14:15:00',
+    ], 'Schedule transition');
+
+    $this->assertSession()->pageTextContains('Scheduled transitions must occur at the top of the hour');
+    $this->assertCount(0, \Drupal::entityTypeManager()->getStorage('scheduled_transition')->loadMultiple());
+  }
+
+  /**
+   * Submitting the add form with a top-of-hour time succeeds.
+   */
+  public function testAddFormAcceptsTopOfHourTime(): void {
+    $this->drupalGet($this->node->toUrl('scheduled_transition_add'));
+
+    $date = new \DateTime('+1 day');
+    $this->submitForm([
+      'revision' => 'latest_revision',
+      'transition' => 'publish',
+      'on[date]' => $date->format('Y-m-d'),
+      'on[time]' => '15:00:00',
+    ], 'Schedule transition');
+
+    $this->assertSession()->pageTextContains('Scheduled a transition for');
+    $transitions = \Drupal::entityTypeManager()->getStorage('scheduled_transition')->loadMultiple();
+    $this->assertCount(1, $transitions);
+    /** @var \Drupal\scheduled_transitions\Entity\ScheduledTransitionInterface $transition */
+    $transition = reset($transitions);
+    $this->assertSame('00', (new \DateTime('@' . $transition->getTransitionTime()))->format('i'));
+  }
+
+  /**
+   * Rescheduling an existing transition to a non-hour time is rejected.
+   */
+  public function testRescheduleFormRejectsNonHourTime(): void {
+    $scheduledTransition = ScheduledTransition::create([
+      'entity' => [$this->node],
+      'entity_revision_id' => $this->node->getRevisionId(),
+      'author' => [$this->loggedInUser->id()],
+      'workflow' => 'standard_workflow',
+      'moderation_state' => 'published',
+      'transition_on' => (new \DateTime('+1 day 16:00'))->getTimestamp(),
+    ]);
+    $scheduledTransition->save();
+
+    $this->drupalGet($scheduledTransition->toUrl('reschedule-form'));
+    $this->assertSession()->statusCodeEquals(200);
+
+    $date = new \DateTime('+2 days');
+    $this->submitForm([
+      'date[date]' => $date->format('Y-m-d'),
+      'date[time]' => '09:30:00',
+    ], 'Reschedule transition');
+
+    $this->assertSession()->pageTextContains('Scheduled transitions must occur at the top of the hour');
+  }
+
+}
